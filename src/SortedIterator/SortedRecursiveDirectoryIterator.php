008988be9afd167ba287cb4034f6643dad94c5fc
alessandro stoppato <alexrah@gmail.com>
2024-02-13 14:25:18 +0100

    SortedRecursiveDirectoryIterator.php: add method to loop over an array of dir paths and include_once sorted list of files

---
+++ src/SortedIterator/SortedRecursiveDirectoryIterator.php
@@ -0,0 +1,39 @@
+<?php
+namespace SortedIterator;
+
+class SortedRecursiveDirectoryIterator {
+
+	/**
+	 * @param array $aDirs array of absolute path dirs
+	 */
+	public function __construct(array $aDirs) {
+
+		try {
+			$aFiles = [];
+
+			foreach ($aDirs as $sDir){
+				$oDirectory = new \RecursiveDirectoryIterator( $sDir );
+				$oIterator   = new \RecursiveIteratorIterator($oDirectory);
+				$oIteratorSorted = new SortedIterator($oIterator);
+
+				/** @var \SplFileInfo $oInfo */
+				foreach ($oIteratorSorted as $oInfo) {
+					if ( $oInfo->getFilename() == "." || $oInfo->getFilename() == "..") continue;
+
+					$aFiles[] = $oInfo->getPathName();
+				}
+			}
+
+			foreach ($aFiles as $sFile)
+			{
+				include_once $sFile;
+			}
+		} catch (\Exception $exception){
+
+			error_log(__FILE__ . ":" . $exception->getMessage() );
+
+		}
+
+	}
+
+}
